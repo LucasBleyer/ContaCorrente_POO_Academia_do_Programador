0c7328b9fa3cf922acd3871156ea5d7341b7133a
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-09 16:06:29 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
